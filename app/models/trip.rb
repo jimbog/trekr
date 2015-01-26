d50af143f19bdc9618c83cf04cbe0d8d3e5eaa8b
kooshanajm <kooshanajm@gmail.com>
2015-01-26 15:21:06 -0800

    Trip price range now passing

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,5 @@ class Trip < ActiveRecord::Base
 	validates :name, presence: true
 	validates :location, presence: true
 	validates :length_trip, presence: true
+	validates :price_range, presence: true
 end
