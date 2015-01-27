69d56a3bfe7113377c36b985a592e2c0370859cb
Elaine Lee <elaneylee@gmail.com>
2015-01-27 10:41:21 -0800

    add ActiveModel has secure password option

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include ActiveModel::SecurePassword
+  field :name, type: String
+  field :email, type: String
+  field :password_digest, type: String
+  has_secure_password
+  attr_reader :password
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
@@ -14,4 +20,8 @@ class User < ActiveRecord::Base
         user.password = Devise.friendly_token[0,20]
       end
   end
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
