f9351f44af1f16ffc91495b59a9455a714819ae8
Elaine Lee <elaneylee@gmail.com>
2015-01-28 15:55:24 -0800

    add to edit user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+
 	def index
 		
 	end
@@ -14,7 +16,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to user_path(current_user)
+			redirect_to user_path(current_user), :notice => "Thanks for signing up! Get ready and starting TREKR-ing!"
 		else
 			render :new
 		end
