1e6f618b20f127d7d3fc93ee1409c07092f24ce0
neocodermatrix <goldenroding@yahoo.com>
2015-01-27 17:07:39 -0800

    all model tests passed.  controller test ing progress.  authenticate problem in controller passed by add def authenicate user in application_controler.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   root 'users#index'
   
+  get '/activities' => 'activities#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ spec/factories/activities.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :activity do
+    category "MyString"
+	name "MyString"
+	address "MyString"
+	date_start "2015-01-26 16:03:45"
+	date_end "2015-01-26 16:03:45"
+	description "MyText"
+	cost 1.5
+		end
+
+end
