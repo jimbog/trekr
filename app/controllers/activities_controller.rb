8323829d4664ca79d31f27a643736493f122610b
neocodermatrix <goldenroding@yahoo.com>
2015-01-27 20:28:55 -0800

    successful index controller tests

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,8 +1,24 @@
 class ActivitiesController < ApplicationController
 
 	def index
+		@activities = Activity.all
 	end
 
+	def new
+		@activity = Activity.new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 
 
 end
--- spec/activity/activities_controller_spec.rb
+++ spec/activity/activities_controller_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ActivitiesController, :type => :controller do 
+	
 	describe "GET #index" do 
 
 		it "responds to successfully with an HTTP 200 status code" do 
@@ -8,6 +9,21 @@ RSpec.describe ActivitiesController, :type => :controller do
 			get :index
 			expect(response).to be_success
 			expect(response).to have_http_status(200)
+		end
+
+		it "assigns @activities" do
+			activity = FactoryGirl.create(:activity)
+			get :index
+			expect(assigns(:activities)).to eq([activity])
+		end
+	end
+
+	describe "GET #new" do
+
+		it "assigns @activity" do
+			activity = FactoryGirl.create(:activity)
+			get :new
+			expect(assigns(:activity)).to eq([activity])
 		end
 	end
 end
