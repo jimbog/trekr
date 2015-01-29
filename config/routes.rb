5c25e76ce24ab71157eda514c828af19246d4da2
Elaine Lee <elaneylee@gmail.com>
2015-01-28 20:32:23 -0800

    name and home_city saved to database on edit page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root "users#index"
   # resources :users
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  get "/users/edit" => "users#edit"
+  # get "/users/edit" => "users#edit"
 
 end
