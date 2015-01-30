0fd23b79f477ebea3e017850850f55130b889733
Elaine Lee <elaneylee@gmail.com>
2015-01-29 19:16:20 -0800

    add api and api users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,13 @@
+module Api
+
+	class UsersController < ApplicationController
+			protect_from_forgery with: :null_session
+			respond_to :json
+
+		def index
+			users = User.all 
+
+			respond_with users
+		end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # get "/users/edit" => "users#edit"
   get "/users/show" => "users#show"
+
+  namespace :api do
+    resources :users do
+        resources :trips, only: [:index, :show], defaults: { format: "json"}
+      end
+  end
 end
