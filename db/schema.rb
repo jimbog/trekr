3fb632d8fdf29e82fab3201ef2b8165d429f3c53
Koosha Araghi <kooshanajm@gmail.com>
2015-02-02 05:29:49 -0800

    Index view styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202095626) do
+ActiveRecord::Schema.define(version: 20150202131246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150202095626) do
     t.integer  "users_id"
     t.integer  "trips_id"
     t.string   "avatar"
+    t.string   "description"
   end
 
   create_table "trips_users", id: false, force: true do |t|
